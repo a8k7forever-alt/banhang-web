cf101cf343e825469f4487c2d73102ce08b9cc4b
BanhangUser <banhang@example.com>
2025-10-08 14:29:15 +0700

    Disable ESLint and TypeScript errors during build for production

--- next.config.js
+++ next.config.js
@@ -1,16 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
+    // Disable ESLint during builds
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: false,
+    // Disable TypeScript errors during builds
+    ignoreBuildErrors: true,
   },
 }
 
